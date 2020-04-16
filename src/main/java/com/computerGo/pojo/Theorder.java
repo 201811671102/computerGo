bde39c031472b313368ded805d5aff38ecb23022
ganchen <QQ519706545001>
2020-04-16 22:13:02 +0800

    20200416-05

---
+++ src/main/java/com/computerGo/pojo/Theorder.java
@@ -0,0 +1,65 @@
+package com.computerGo.pojo;
+
+import java.util.Date;
+
+public class Theorder {
+    private Integer oid;
+
+    private Date ordertime;
+
+    private Integer pid;
+
+    private Integer num;
+
+    private Integer state;
+
+    private String uaddress;
+
+    public Integer getOid() {
+        return oid;
+    }
+
+    public void setOid(Integer oid) {
+        this.oid = oid;
+    }
+
+    public Date getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(Date ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getUaddress() {
+        return uaddress;
+    }
+
+    public void setUaddress(String uaddress) {
+        this.uaddress = uaddress == null ? null : uaddress.trim();
+    }
+}
